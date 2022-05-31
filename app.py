1f712a259aadf994149731b94899df128648a847
Nicole-Alicia <nixaly17@gmail.com>
2022-05-31 23:05:33 +0000

    aula 4

--- app.py
+++ app.py
@@ -25,5 +25,10 @@ def exibir_entradas():
     entradas = []
     for titulo, texto, criado_em in cur.fetchall():
         entradas.append({"titulo": titulo, "texto": texto, "criado_em": criado_em})
-    return render_template("layout.html", entradas=entradas)
+    return render_template("exibir_entradas.html", entradas=entradas)
 
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+@app.route
